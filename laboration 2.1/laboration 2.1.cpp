a379fb039e32a6652f5ebacc69a59738eb1752a4
DonPedro228 <Roman.Kos.RI.2023@lpnu.ua>
2023-09-27 16:47:41 +0300

    first master commit

---
+++ laboration 2.1/laboration 2.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double z1;
+	//double z2;
+	double a;
+	double b;
+	cin >> a >> b;
+	z1 = (sin(a) + cos(2 * b - a)) / (cos(a) - sin(2 * b - a));
+	//z2 = (1 + sin(b) * sin(b)) / (cos(b) * cos(b));
+	cout << "Z1 = " << z1 << endl;
+	//cout << "Z2 = " << z2 << endl;
+	return 0;
+}
